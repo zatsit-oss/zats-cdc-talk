2952b282282386805e139fcff14ca1246d002104
Ludovic Dussart <ludovic.dussart.pro@gmail.com>
2025-09-03 13:53:34 +0200

    fix: tailwind usages

--- package/frontend/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    // Use require to ensure PostCSS loads the plugin correctly
-    '@tailwindcss/postcss': require('@tailwindcss/postcss'),
-    autoprefixer: require('autoprefixer'),
-  },
-}
